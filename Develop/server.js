4aa18a984add9f0f1b48d5428a8af454b8ab9bbd
Bennet Wilson <bennetwilson@gmail.com>
2022-01-11 20:10:10 -0700

    made it work. added a lot server side

--- Develop/server.js
+++ Develop/server.js
@@ -1,30 +1,53 @@
 // requires
 const express = require('express');
-const { get } = require('express/lib/response');
+const db = require('./db/db.json')
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+// const { get } = require('express/lib/response');
 // variables
 const path = require('path')
 const PORT = 3001
 const app = express();
 
+// Intercode? Or something
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // code
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /notes or /index'));
+// app.get('/', (req, res) => res.send('Navigate to /notes or /index'));
 
-app.get('/index', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-);
-app.post('/index', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+
+app.get('/api/notes', (req, res) => {
+    res.json(db)
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuidv4();
+    db.push(req.body)
+    fs.writeFileSync('./db/db.json', JSON.stringify(db,null,'\t'))
+    res.json(db)
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id === req.params.id){
+            db.splice(i,1)
+        }
+    }
+    fs.writeFileSync('./db/db.json', JSON.stringify(db,null,'\t'))
+    res.json(db)
+})
 
 app.listen(PORT, () =>
 console.log(`Listening at http://localhost:${PORT}`)
 );
+

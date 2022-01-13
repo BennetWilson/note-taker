e6839eefef24f62164bbc4e07e4cc252d91b31a0
Bennet Wilson <bennetwilson@gmail.com>
2022-01-13 10:21:23 -0700

    added additional port

--- Develop/server.js
+++ Develop/server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 
 // variables
 const path = require('path')
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Intercode? Or something
